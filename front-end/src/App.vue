bd83f51b56e5f5e1f6df3d3e5ecb94ce69708e6f
SimoneVilardo <simovila95@gmail.com>
2023-12-14 13:27:59 +0100

    creata pagina index esercio day 2

---
+++ front-end/src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { onMounted, ref } from 'vue';
+import axios from 'axios';
+
+import PizzaIndex from './components/PizzaIndex.vue';
+
+const pizzas = ref(null);
+
+const updatePizzas = async () => {
+  const data = await axios.get("http://localhost:8080/api/v1.0/pizzas");
+  pizzas.value = data.data;
+};
+
+onMounted(updatePizzas);
+</script>
+
+
+<template>
+  <div>
+    <pizza-index :pizzas="pizzas"/>
+  </div>
+</template>
---
+++ front-end/src/components/PizzaIndex.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { defineProps } from 'vue';  
+
+const props = defineProps({
+    pizzas: {
+        type: Array,
+        required: true
+    }
+});
+</script>
+<template>
+    <div>
+        <ul>
+            <h1>Pizze</h1>
+            <li 
+                class="list-elem"
+                v-for="pizza in pizzas" 
+                :key="pizza.id"
+            >
+                {{ pizza.nome }}
+            </li>
+        </ul>
+    </div>
+</template>
+<style scoped>
+    
+</style>
